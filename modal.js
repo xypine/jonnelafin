06627f68c0823c7028969bbc5b3eb450cbfda6e7
Elias Eskelinen <elias.eskelinen@pm.me>
2020-11-28 14:22:41 +0200

    downscales

--- modal.js
+++ modal.js
@@ -7,7 +7,11 @@ function a(elem) {
 	openInNewTab(elem.id)
 }
 function onClick(element) {
-	openInNewTab(element.src)
+	var url = element.src;
+	url = url.replace("downscaled_to_50", "normal");
+	url = url.replace("downscaled_to_30", "normal");
+	url = url.replace("downscaled_to_5", "normal");
+	openInNewTab(url);
 //	document.getElementById("img01").src = element.src;
 //	document.getElementById("modal01").style.display = "block";
 //	var captionText = document.getElementById("caption");
